d3c2561bb74e8d56e420677bd08f27adc6e51d05
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2023-11-15 18:05:05 +0100

    add function to handle push button "Generate report" action

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow, QDialog
+import os
+
+from PyQt6.QtWidgets import QMainWindow, QDialog, QFileDialog
 from Views.Main.main_window import Ui_MainWindow_Main
 from Views.Settings.settings_general import Ui_Dialog_App_Settings
 from Views.Settings.report_content import Ui_Dialog_Report_Content
@@ -14,6 +16,7 @@ class MainController(QMainWindow, Ui_MainWindow_Main):
         self.action_App_Settings.triggered.connect(self.windowAppSettings)
         self.action_Raport_Settings.triggered.connect(self.windowReportContent)
         self.action_Exit.triggered.connect(self.close)
+        self.pushButton_Generate_Report.clicked.connect(self.generateReport)
 
         self.show()
 
@@ -28,3 +31,22 @@ class MainController(QMainWindow, Ui_MainWindow_Main):
         self.window_report_content_ui = Ui_Dialog_Report_Content()
         self.window_report_content_ui.setupUi(self.window_report_content)
         self.window_report_content.show()
+
+    def generateReport(self):
+        try:
+            fileFilter = ('Plik PDF (*.pdf)')
+
+            filePath = QFileDialog.getSaveFileName(
+                caption="Eksportuj plik",
+                directory=os.path.expanduser("~/Desktop/raport.pdf"),
+                filter=fileFilter,
+                initialFilter='Plik PDF (*.pdf)')
+
+            filePath = str(filePath[0])
+
+            if filePath.endswith(".pdf"):
+                # TODO add function to handle generate contents of report
+                print(filePath)
+
+        except Exception as e:
+            print(e)
