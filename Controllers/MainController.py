2b008eec34622e68529b21254bb584ee070bb57c
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2023-11-15 17:41:05 +0100

    add new ui

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QDialog
 from Views.Main.main_window import Ui_MainWindow_Main
+from Views.Settings.settings_general import Ui_Dialog_App_Settings
+from Views.Settings.report_content import Ui_Dialog_Report_Content
 
 
 class MainController(QMainWindow, Ui_MainWindow_Main):
@@ -7,5 +9,22 @@ class MainController(QMainWindow, Ui_MainWindow_Main):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # Connections
+        self.action_App_Settings.triggered.connect(self.windowAppSettings)
+        self.action_Raport_Settings.triggered.connect(self.windowReportContent)
+        self.action_Exit.triggered.connect(self.close)
+
         self.show()
 
+    def windowAppSettings(self):
+        self.window_app_settings = QDialog()
+        self.window_app_settings_ui = Ui_Dialog_App_Settings()
+        self.window_app_settings_ui.setupUi(self.window_app_settings)
+        self.window_app_settings.show()
+
+    def windowReportContent(self):
+        self.window_report_content = QDialog()
+        self.window_report_content_ui = Ui_Dialog_Report_Content()
+        self.window_report_content_ui.setupUi(self.window_report_content)
+        self.window_report_content.show()
